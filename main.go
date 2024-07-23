d6c6a69c284e41ba98484cdf7a50dcbb71e32edb
cjbruin23 <colinjb23@gmail.com>
2024-07-22 20:14:42 -0700

    new handler; global db variable

--- main.go
+++ main.go
@@ -11,13 +11,26 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 func CheckError(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query(`select * from profile`)
+	CheckError(err)
+
+	for rows.Next() {
+		var id int
+		var username string
+		var info string
+		err := rows.Scan(&id, &username, &info)
+		CheckError(err)
+		fmt.Println(username)
+	}
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
@@ -29,25 +42,12 @@ func main() {
 
 	psqlconn := os.Getenv("DB_HOST")
 
-	db, err := sql.Open("postgres", psqlconn)
+	db, err = sql.Open("postgres", psqlconn)
 
 	CheckError(err)
 
-	rows, e := db.Query(`select * from profile`)
-
-	for rows.Next() {
-		var id int
-		var username string
-		var info string
-		err = rows.Scan(&id, &username, &info)
-		CheckError(err)
-		fmt.Println(username)
-	}
-
-	CheckError(e)
-
 	// close database
 	defer db.Close()
-	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/user/", userHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
