335b56b81657bffb32b1a25a20d0b00a04005017
cjbruin23 <colinjb23@gmail.com>
2024-07-26 15:14:59 -0700

    adding in cors to recieve a local frontend request

--- dist/index.js
+++ dist/index.js
@@ -2,16 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require('cors');
 // configures dotenv to work in your application
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+//CORS
+const corsOptions = {
+    origin: ['http://localhost:5173']
+};
+app.use(cors(corsOptions));
+// ROUTES
 app.get("/", (request, response) => {
     response.status(200).send("Hello World");
 });
 app.post('/player', (request, response) => {
+    console.log('request', request);
     response.status(200).send("Hello World");
 });
+// SERVER START
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log("Server running at PORT: ", PORT);
 }).on("error", (error) => {
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,32 @@ import { Request, Response } from "express";
 
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require('cors')
 
 // configures dotenv to work in your application
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
+//CORS
+const corsOptions = {
+  origin: ['http://localhost:5173']
+}
+app.use(cors(corsOptions))
 
+
+// ROUTES
 app.get("/", (request: Request, response: Response) => { 
   response.status(200).send("Hello World");
 }); 
 
 app.post('/player', (request: Request, response: Response) => {
+  console.log('request', request)
   response.status(200).send("Hello World");
-})
+});
 
+
+// SERVER START
+const PORT = process.env.PORT;
 app.listen(PORT, () => { 
   console.log("Server running at PORT: ", PORT); 
 }).on("error", (error: any) => {
